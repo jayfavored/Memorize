c2eea0b7d587751881d6fe05250af6c0121d8ffc
Jianhua Huang <jayfavored@outlook.com>
2025-03-19 20:53:10 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jay Huang on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
